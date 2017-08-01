91b97f8e3ddb2db342cf1c02381f0f5c50b0ba85
Dil2000 <dnks2000@gmail.com>
2017-08-01 19:24:36 -0400

    2nd

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-	var http = require('http')
+	var http = require('http');
 
-	var PORT = 8080;
+	var PORT1 = 7000;
+	var PORT2 = 7500;
 
 	function handleRequest(req,res){
-		res.end('Server works' + req.url);
+		res.end('Server works YAY' + req.url);
 	}
 
 	var server = http.createServer(handleRequest);
 
-	server.listen(PORT,function(){
-		console.log(`server listening on : http://localhost:${PORT}`);
+	server.listen(PORT1,function(){
+		console.log(`Server works  : http://localhost:${PORT}`);
 	});
+
+
+	function handleRequest2(req,res){
+		res.end('Server does not work' + req.url);
+	}
+
+	var server = http.createServer(handleRequest2);
+
+	server.listen(PORT2,function(){
+		console.log(`Server does not work : http://localhost:${PORT}`);
+	});
+
+
