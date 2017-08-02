64a8f3f922ed152208558090d4a906c50d0cf4af
Dil2000 <dnks2000@gmail.com>
2017-08-02 11:11:50 -0400

    all

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
-// Dependencies
+// We require/import the HTTP module
 var http = require("http");
 
-var PORT = 8080;
+// =====================================================================
 
-var server = http.createServer(function(req, res) {
+// Then define the ports we want to listen to
+var PORTONE = 7000;
+var PORTTWO = 7500;
 
-  // Saving the request method as a variable.
-  var requestData = "";
+// =====================================================================
 
-  // When the server receives data, it will add it to requestData.
-  req.on("data", function(data) {
-    requestData += data;
-    console.log("You just posted some data to the server!");
-    console.log("Your data was " + requestData);
-  });
+// We need two different functions to handle requests, one for each server.
+function handleRequestOne(request, response) {
+  response.end("You're a JavaScript mastermind!");
+}
 
-  // When the request has ended...
-  req.on("end", function() {
-    res.write("<html><head><title>Hello Noder!</title></head><body>");
-    res.write("<h1>Thank You!</h1>");
-    res.write("</body></html>");
-    res.end();
-  });
+function handleRequestTwo(request, response) {
+  response.end("You smell.");
+}
 
+// =====================================================================
+
+// Create our servers
+var serverOne = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
+
+// =====================================================================
+
+// Starting our servers
+serverOne.listen(PORTONE, function() {
+
+  // Callback triggered when server is successfully listening. Hurray!
+  console.log("Server listening on: http://localhost:%s", PORTONE);
 });
 
-// Starts our server.
-server.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:%s", PORT);
+serverTwo.listen(PORTTWO, function() {
+
+  // Callback triggered when server is successfully listening. Hurray!
+  console.log("Server listening on: http://localhost:%s", PORTTWO);
 });
